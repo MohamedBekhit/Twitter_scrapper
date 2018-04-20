3bbab0d7521fd1cf99df349904ae7ea94247eb1c
Mohamed Bekhit <mohamedbekhit.mg@gmail.com>
2018-04-20 19:26:40 +0200

    Create Hidden.py

---
+++ Hidden.py
@@ -0,0 +1,9 @@
+def oauth():
+"""
+Replace the '##' by the appropriate fields acquired from Twitter's API. These data fields are confidential and you should always
+keep them hidden.
+"""
+	return {"consumer_key" : "##", "consumer_secret" : "##"
+			, "token_key" : "##",
+			"token_secret" : "##"}
+      
